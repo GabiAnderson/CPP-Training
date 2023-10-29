468bb4c43de8e457f8ffd1e57695c5c3ff4131df
GabiAnderson <54378358+GabiAnderson@users.noreply.github.com>
2023-10-28 23:44:29 -0600

    created project template laptop and desktop (variations)

--- 3.5 Statements and Functions/main.cpp
+++ 3.5 Statements and Functions/main.cpp
@@ -7,12 +7,36 @@
     Execution continues until there is a statement causing the program to terminate, or run another set of statements
 
     FUNCTIONS
+    A function is like a machine, you give it input and it provides an expected output
+    CPP has a special syntax to define functions
+        returnType functionName (paramterType paramterName,...,)
+        {
+            function body;
+            return val; val must match returnType
+        }
+    Functions need to be defined before they can be used
 */
 
 #include <iostream>
 
+// This is a function - defined before it is used and matching the syntax
+int addNumbers (int first_number, int second_number)
+{
+    int sum = first_number + second_number;
+    return sum;
+}
+
 int main()
 {
-    std::cout << "Hello World!" << std::endl; // this is a statement
+    int firstNumber = 12; // this is a statement
+    int secondNumber = 9;
+
+    int sum = firstNumber + secondNumber;
+
+    // this is an example of using (calling) the predefined function from above
+    sum = addNumbers(firstNumber, secondNumber); // this is a function call - also a statement
+
+    sum = addNumbers(3, 4); // this is another statement - and another function call
+
     return 0; // this is also a statement
 }
